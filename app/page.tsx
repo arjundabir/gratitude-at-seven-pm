74dcf21858c38fc0fb19041bab49210131c6b38d
Arjun Dabir <arjunadabir@gmail.com>
2025-01-02 02:56:55 -0800

    build and lint fixes

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <div className="h-screen w-screen flex items-center justify-center">
-      <p>I'm grateful for the new year.</p>
+      <p>I&apos;m grateful for the new year.</p>
     </div>
   );
 }
