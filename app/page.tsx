6a2cc0f56da49b3406a2b61c2d3e0b4c67379e26
Arjun Dabir <arjunadabir@gmail.com>
2025-01-02 20:12:48 -0800

    01/02/2025

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="h-screen w-screen flex items-center justify-center">
-      <p>I&apos;m grateful for the new year.</p>
+      <p>I&apos;m grateful for the friends I've made.</p>
     </div>
   );
 }
